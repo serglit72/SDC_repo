0fe0d347060204373cf686f32e6f2843dcb14df2
Sergei Litovchenko <serglit72@gmail.com>
2017-07-23 17:23:50 -0700

    Project 1- Finding lane line

---
+++ Find_Line_recognizing.py
@@ -0,0 +1,69 @@
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import tensorflow as tf
+%matplotlib inline
+
+
+image = mpimg.imread('images/solidYellowLeft.jpg')
+
+def process_image(image):
+    # Read in and grayscale the image
+    gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+    # Define a kernel size and apply Gaussian smoothing
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+    # Define our parameters for Canny and apply
+    low_threshold = 50
+    high_threshold = 150
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    # Next we'll create a masked edges image using cv2.fillPoly()
+    mask = np.zeros_like(edges)   
+    ignore_mask_color = 255   
+
+    # This time we are defining a four sided polygon to mask
+    imshape = image.shape
+    vertices = np.array([[(0,imshape[0]),(480, 310), (490, 310), (imshape[1],imshape[0])]], dtype=np.int32)
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    masked_edges = cv2.bitwise_and(edges, mask)
+    
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 2 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 50    # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 10 #minimum number of pixels making up a line
+    max_line_gap = 15    # maximum gap in pixels between connectable line segments
+    line_image = np.copy(image) # creating a blank to draw lines on
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                                min_line_length, max_line_gap)
+
+    # Iterate over the output "lines" and draw lines on a blank image
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_image,(x1,y1),(x2,y2),(0,255,0),10)
+
+    # Create a "color" binary image to combine with line image
+    color_edges = np.dstack((edges, edges, edges)) 
+
+    # Draw the lines on the edge image
+    lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
+
+    return lines_edges
+    
+
+new_clip_output = 'Video/my_output.mp4'
+test_clip = VideoFileClip('Video/solidYellowLeft.mp4').subclip(0,5)
+#new_clip = test_clip.fl_image(lambda x: cv2.cvtColor(x, cv2.COLOR_RGB2YUV)) #NOTE: this function expects color images!!
+my_clip = test_clip.fl_image(process_image)
+%time my_clip.write_videofile(new_clip_output, audio=False)
+
